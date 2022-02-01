c104d5b062888560e96a0dd83bb415ffe8f80ba2
WanPeng <986063761@qq.com>
2022-02-01 22:34:20 +0800

    更新

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-layout class="root-wrap">
+    <a-layout-sider
+      collapsible>
+      <a-avatar :src="avatar" :size="70"/>
+    </a-layout-sider>
+    <a-layout-content>
+      
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script>
+import avatar from './assets/avatar.jpg'
+export default {
+  data() {
+    return {
+      avatar
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.root-wrap {
+  height: 100vh;
+  .ant-avatar {
+    display: block;
+    margin: 15px auto;
+  }
+  .ant-layout-content {
+    overflow: auto;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'ant-design-vue/dist/antd.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import Antd from 'ant-design-vue'
+
+createApp(App)
+  .use(Antd)
+  .mount('#app')
