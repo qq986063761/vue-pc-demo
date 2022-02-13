9d70a8883c3ca0f261c8509c2264889c1dd03e75
WanPeng <986063761@qq.com>
2022-02-13 22:33:30 +0800

    更新

---
+++ src/directives/keyboard-select.js
@@ -0,0 +1,57 @@
+export default {
+  bind(el, {value}) {
+    const {index, itemClass, onChange, onEnter} = value
+    el._keyboardSelect = {
+      isShow: false,
+      index,
+      onKeyDown(event) {
+        const isShow = !!(el.offsetWidth && el.offsetHeight)
+        if (isShow && !el._keyboardSelect.isShow) el._keyboardSelect.index = -1
+        el._keyboardSelect.isShow = isShow
+        if (el._keyboardSelect.isShow) {
+          const code = event.keyCode || event.which
+          switch (code) {
+            case 38:
+            case 40:
+              let currentIndex = el._keyboardSelect.index
+              if (code === 38) {
+                currentIndex--
+                if (currentIndex < 0) return
+              } else {
+                currentIndex++
+              }
+              // 对看不到的元素重新滚动定位到指定位置
+              if (itemClass) {
+                const children = el.querySelectorAll('.' + itemClass)
+                const rsEl = children && children[currentIndex]
+                if (rsEl) {
+                  const rect = el.getBoundingClientRect()
+                  const rsElRect = rsEl.getBoundingClientRect()
+                  if (rsElRect.top >= rect.bottom) {
+                    el.scrollTop += rsElRect.height * 2
+                  } else if (rsElRect.bottom <= rect.top) {
+                    el.scrollTop -= rsElRect.height * 2
+                  }
+                }
+              }
+              onChange && onChange({
+                index: currentIndex
+              })
+              break
+            case 13:
+              onEnter && onEnter()
+              break
+          }
+        }
+      }
+    }
+    document.addEventListener('keydown', el._keyboardSelect.onKeyDown)
+  },
+  componentUpdated(el, {value}, vnode, oldVnode) {
+    const {index} = value
+    el._keyboardSelect.index = index
+  },
+  unbind(el) {
+    document.removeEventListener('keydown', el._keyboardSelect.onKeyDown)
+  }
+}
